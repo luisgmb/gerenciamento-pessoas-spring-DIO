5b6d3f27981d502a7fb8e49c47c5909d3f212fd8
luisgmb <luisgmbarcelos@gmail.com>
2021-11-08 16:03:02 -0300

    Development of unit tests

--- personalapi/src/test/java/one/digitalinnovation/personalapi/service/PersonServiceTest.java
+++ personalapi/src/test/java/one/digitalinnovation/personalapi/service/PersonServiceTest.java
@@ -1,4 +1,47 @@
 package one.digitalinnovation.personalapi.service;
 
+import one.digitalinnovation.personalapi.dto.mapper.PersonMapper;
+import one.digitalinnovation.personalapi.dto.request.PersonDTO;
+import one.digitalinnovation.personalapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personalapi.entity.Person;
+import one.digitalinnovation.personalapi.repositories.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+import static one.digitalinnovation.personalapi.utils.PersonUtils.createFakeDTO;
+import static one.digitalinnovation.personalapi.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
+    @Mock
+    private PersonRepository personRepository;
+
+    @Mock
+    private PersonMapper personMapper;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSuccessSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personMapper.toModel(personDTO)).thenReturn(expectedSavedPerson);
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedSuccessMessage(expectedSavedPerson.getId());
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedSuccessMessage(Long savedPersonId) {
+        return MessageResponseDTO.builder().message("Person successfully created with ID " + savedPersonId).build();
+    }
 }
--- personalapi/src/test/java/one/digitalinnovation/personalapi/utils/PersonUtils.java
+++ personalapi/src/test/java/one/digitalinnovation/personalapi/utils/PersonUtils.java
@@ -14,10 +14,11 @@ public class PersonUtils {
     private static final long PERSON_ID = 1L;
 
     public static PersonDTO createFakeDTO(){
-        return PersonDTO.builder().firstName(FIRST_NAME).lastName(LAST_NAME).cpf(CPF_NMBR).birthDate(BIRTH_DATE)
+        return PersonDTO.builder().firstName(FIRST_NAME).lastName(LAST_NAME).cpf(CPF_NMBR).birthDate("15-12-2000")
                 .phones(Collections.singletonList(PhoneUtils.createFakeDTO())).build();
     }
     public static Person createFakeEntity(){
-
+        return Person.builder().id(PERSON_ID).firstName(FIRST_NAME).lastName(LAST_NAME).cpf(CPF_NMBR).birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity())).build();
     }
 }
